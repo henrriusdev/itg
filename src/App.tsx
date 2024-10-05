a85eea2570d57d2affb094e0815db16a76f43978
henrriusdev <henrrybrgt@gmail.com>
2024-10-05 18:23:07 -0400

    Refactor image source in App component and update container width and button size

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,16 @@ function App() {
             initial={{ x: "-100vw", opacity: 0 }} // El contenedor empieza fuera del viewport (a la izquierda)
             animate={{ x: 0, opacity: 1 }} // Se mueve al centro y aparece gradualmente
             transition={{ duration: 1.5, ease: "easeInOut" }} // Transición suave
-            style={{ maxWidth: "35%", width: "100%", marginLeft: '8%' }} // Controlamos el ancho máximo del contenedor
+            style={{ maxWidth: "32%", width: "100%", marginLeft: '8%' }} // Controlamos el ancho máximo del contenedor
           >
-            <Card className="p-4 bg-slate-300" radius="lg">
+            <Card className="p-10 bg-slate-300" radius="lg">
               <CardBody>
-                <h1 className="text-5xl font-medium text-slate-700">
+                <h1 className="text-6xl p-3 px-1 font-medium text-slate-700">
                   Lorem ipsum dolor sit amet
                 </h1>
                 <div className="flex justify-between gap-x-4 p-3">
-                  <div className="border-t-5 border-gray-400 mt-4 !w-[1000px]"></div>
-                  <p className="mt-2 text-sm text-slate-500">
+                  <div className="border-t-5 border-gray-400 mt-4 !w-[600px]"></div>
+                  <p className="mt-2 text-lg text-slate-500">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
                     do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
@@ -39,9 +39,16 @@ function App() {
             </Card>
           </motion.div>
         </div>
-        <Button className="w-auto ml-[64%]" variant="ghost" color="primary">
+        <Button className="w-auto ml-[64%]" variant="ghost" color="primary" size="md">
           Learn more
         </Button>
+      </div>
+      <div className="flex justify-center flex-col items-start bg-gradient-to-l from-blue-950 to-gray-600 p-4">
+        <div className="w-1/2 mx-auto">
+        <h2 className="text-5xl font-medium text-gray-200 py-3">Our Services</h2>
+        <p className="text-gray-200 text-lg">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate sit odio corrupti nostrum laboriosam necessitatibus, alias in dignissimos sapiente porro fugiat unde consequatur vel blanditiis. Commodi, ullam! Illum, ipsam necessitatibus.
+        Ipsa ducimus minima soluta amet vitae temporibus, quis corrupti, aliquam, veritatis rem ullam expedita tempore repellat!</p>
+        </div>
       </div>
     </>
   );
